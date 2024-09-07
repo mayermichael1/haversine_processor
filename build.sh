03a24f78fd45bb08d518eea7df5f3b8214d916c1
mayermichael1 <michael.mayer2105@gmail.com>
2024-09-08 00:39:42 +0200

    time build time

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 WARNING="-Wall -Werror -Wextra -Wno-unused-variable"
 BUILD_DIR="./output"
 
-bear -- g++ -O0 -g gen_json.cpp -o $BUILD_DIR/generate_json.out
+time bear -- g++ -O0 -g gen_json.cpp -o $BUILD_DIR/generate_json.out
 
 
