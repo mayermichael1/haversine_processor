203953fe120b2fe29591dff0adf23f85b07d4a30
mayermichael1 <michael.mayer2105@gmail.com>
2024-09-14 00:56:25 +0200

    read whole json file to memory

--- process/process_haversine.cpp
+++ process/process_haversine.cpp
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int
-main ()
+#include "types.h"
+
+s32
+main (s32 argc, u8 **argv)
 {
-  printf("hello world\n");
+  u8 *json_file_name = NULL;
+  s32 json_size = 0;
+  u8 *json_memory = NULL;
+
+  if (argc >= 2)
+    {
+      json_file_name = argv[1];
+    }
+
+  FILE *fp = fopen(json_file_name, "rb");
+  if (fp)
+    {
+      fseek(fp, 0, SEEK_END);
+      json_size = ftell(fp); 
+      rewind(fp);
+      json_memory = (u8*)malloc(json_size);
+      fread(json_memory, sizeof(u8), json_size, fp);
+      fclose(fp);
+    }
+
+  if (json_size > 10)
+    {
+      printf("%c \n", json_memory[(json_size-2)]);
+    } 
   return 0;
 }
