1efa9cf19be72b7d3bfc82c9c24eb2f976884554
mayermichael1 <michael.mayer2105@gmail.com>
2024-09-15 12:39:51 +0200

    switch out switch cases for a if

--- process/process_haversine.cpp
+++ process/process_haversine.cpp
@@ -24,6 +24,28 @@ read_file (u8 *file_name, s32 *size)
   return memory;
 }
 
+static bool
+str_contains_any(u8* str, u8* characters)
+{
+  b8 result = false;
+  u8* cursor = str;
+  u8 character = 0;
+
+  while ((character = (*cursor++)) != 0)
+    {
+      u8* find_cursor = characters;
+      u8 compare_character = 0;
+      while ((compare_character = (*find_cursor++)) != 0)
+        {
+          if (character == compare_character)
+            {
+              result = true;
+            }
+        }
+    }
+  return result;
+}
+
 s32
 main (s32 argc, u8 **argv)
 {
@@ -54,79 +76,66 @@ main (s32 argc, u8 **argv)
       current_word[current_word_index] = character;
       current_word_index++;
 
-      switch (character)
+      if (str_contains_any(current_word, (u8*)" \n\r\t,{}[]:"))
         {
-          case ' ':
-          case '\n':
-          case '\r':
-          case '\t':
-          case ',':
-          case '{':
-          case '}':
-          case '[':
-          case ']':
-          case ':':
-            {
-              current_word_index--;
-              current_word[current_word_index] = 0;
+          current_word_index--;
+          current_word[current_word_index] = 0;
 
 #if 0
-              if (strncmp(current_word, "null", 4) == 0)
-                {
-                  //
-                }
-              else if (strncmp(current_word, "true", 4) == 0)
-                {
-                  //
-                }
-              else if (strncmp(current_word, "false", 5) == 0)
-                {
-                  // 
-                }
+          if (strncmp(current_word, "null", 4) == 0)
+            {
+              //
+            }
+          else if (strncmp(current_word, "true", 4) == 0)
+            {
+              //
+            }
+          else if (strncmp(current_word, "false", 5) == 0)
+            {
+              // 
+            }
 #endif
-              if (current_word[0] == '"' &&
-                       current_word[current_word_index - 1] == '"')
+          if (current_word[0] == '"' &&
+                   current_word[current_word_index - 1] == '"')
+            {
+              current_word[current_word_index - 1] = 0;
+              strcpy(last_identifier, &current_word[1]);
+            }
+          else 
+            {
+              u8* endptr;
+              f64 value = strtod(current_word, &endptr); 
+              if (endptr != NULL &&
+                  endptr != current_word)
                 {
-                  current_word[current_word_index - 1] = 0;
-                  strcpy(last_identifier, &current_word[1]);
-                }
-              else 
-                {
-                  u8* endptr;
-                  f64 value = strtod(current_word, &endptr); 
-                  if (endptr != NULL &&
-                      endptr != current_word)
+                  if (strncmp(last_identifier, "x0", 2) == 0)
                     {
-                      if (strncmp(last_identifier, "x0", 2) == 0)
-                        {
-                          coords[0].longitude = value;
-                        }
-                      else if (strncmp(last_identifier, "x1", 2) == 0)
-                        {
-                          coords[1].longitude = value;
-                        }
-                      else if (strncmp(last_identifier, "y0", 2) == 0)
-                        {
-                          coords[0].latitude = value;
-                        }
-                      else if (strncmp(last_identifier, "y1", 2) == 0)
-                        {
-                          coords[1].latitude = value;
-                          haversine_sum += reference_haversine(
-                            coords[0],
-                            coords[1],
-                            EARTH_RADIUS
-                          );
-                          haversine_calc_ammount++;
-                          coords[0] = {0.0, 0.0};
-                          coords[1] = {0.0, 0.0};
-                        }
+                      coords[0].longitude = value;
+                    }
+                  else if (strncmp(last_identifier, "x1", 2) == 0)
+                    {
+                      coords[1].longitude = value;
+                    }
+                  else if (strncmp(last_identifier, "y0", 2) == 0)
+                    {
+                      coords[0].latitude = value;
+                    }
+                  else if (strncmp(last_identifier, "y1", 2) == 0)
+                    {
+                      coords[1].latitude = value;
+                      haversine_sum += reference_haversine(
+                        coords[0],
+                        coords[1],
+                        EARTH_RADIUS
+                      );
+                      haversine_calc_ammount++;
+                      coords[0] = {0.0, 0.0};
+                      coords[1] = {0.0, 0.0};
                     }
                 }
-
-              current_word_index = 0;
-              break;
             }
+
+          current_word_index = 0;
         }
     }
 
