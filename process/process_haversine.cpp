5528777393760be34782e41a6ac86e3d1388c62c
mayermichael1 <michael.mayer2105@gmail.com>
2025-02-03 22:37:15 +0100

    read results file and check against it

--- process/process_haversine.cpp
+++ process/process_haversine.cpp
@@ -26,6 +26,20 @@ get_file_size (u8* file_name)
     return size;
 }
 
+static b8
+check_haversine (f64 reference, f64 result)
+{
+    b8 correct = false;
+    f64 epsilon = 0.000001f;
+
+    f64 diff = result - reference;
+    if (diff > -epsilon && diff < epsilon)
+    {
+        correct = true;
+    }
+    return correct;
+}
+
 static u8*
 read_file (u8 *file_name, s32 file_size)
 {
@@ -63,6 +77,10 @@ main (s32 argc, u8 **argv)
     s32 json_size = 0;
     u8 *json_memory = NULL;
 
+    u8 *results_file_name = NULL;
+    s32 results_file_size = 0;
+    f64 *results = NULL;
+
     s32 cursor = 0;
     s32 current_word_index = 0;
     u8 current_word[256] = "";
@@ -71,10 +89,16 @@ main (s32 argc, u8 **argv)
     coordinate coords[2] = {};
     s32 haversine_calc_ammount = 0;
     f64 haversine_sum = 0;
+    s32 amount_wrong = 0;
 
     if (argc >= 2)
     {
         json_file_name = argv[1];
+    }
+
+    if (argc >= 3)
+    {
+        results_file_name = argv[2];
     }
 
     cpu_frequency = estimate_cpu_frequencies();
@@ -83,6 +107,12 @@ main (s32 argc, u8 **argv)
     TIMED_BANDWITH("read",json_size);
     json_memory = read_file(json_file_name, json_size);
     TIMED_BANDWITH_END("read");
+
+    results_file_size = get_file_size(results_file_name);
+    TIMED_BANDWITH("read_results",results_file_size);
+    results = (f64*)read_file(results_file_name, results_file_size);
+    TIMED_BANDWITH_END("read_results");
+
 
     TIMED_BLOCK("parse");
     while (cursor < json_size)
@@ -135,12 +165,17 @@ main (s32 argc, u8 **argv)
                         {
                             coords[1].latitude = value;
                             TIMED_BANDWITH("calc", 4 * sizeof(f64));
-                            haversine_sum += reference_haversine(
+                            f64 haversine = reference_haversine(
                                 coords[0],
                                 coords[1],
                                 EARTH_RADIUS
                             );
+
+                            amount_wrong += !check_haversine(*(results++), haversine);
+                            
+                            haversine_sum += haversine;
                             TIMED_BANDWITH_END("calc");
+
                             haversine_calc_ammount++;
                             coords[0] = {0.0, 0.0};
                             coords[1] = {0.0, 0.0};
@@ -154,6 +189,7 @@ main (s32 argc, u8 **argv)
     TIMED_BLOCK_END("parse");
 
     printf("Processed average: %f\n", (haversine_sum/(f64)haversine_calc_ammount));
+    printf("Wrong haversine products: %i\n", amount_wrong);
 
     TIMED_BLOCK_END("main");
     print_profiler();
