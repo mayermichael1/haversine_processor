ff2fcda48182dff2d71b6624d5712ec548ac8cb7
mayermichael1 <michael.mayer2105@gmail.com>
2025-02-09 14:41:24 +0100

    restructure haversine calc

--- shared/calc.cpp
+++ shared/calc.cpp
@@ -1,9 +1,6 @@
 #include "calc.h"
 #include "stdio.h"
 
-static f32 max_pow = -MAXFLOAT;
-static f32 min_pow = MAXFLOAT;
-
 static f32 max_sin = -MAXFLOAT;
 static f32 min_sin = MAXFLOAT;
 
@@ -16,20 +13,11 @@ static f32 min_asin = MAXFLOAT;
 static f32 max_sqrt = -MAXFLOAT;
 static f32 min_sqrt = MAXFLOAT;
 
-inline f64 
-mypow (f64 x, f64 y)
+inline f64
+square (f64 x)
 {
     f64 result = 0;
-    if (x < min_pow)
-    {
-        min_pow = x;
-    }
-    else if (x > max_pow)
-    {
-        max_pow = x;
-    }
-
-    result = pow(x, y);
+    result = x * x;
     return result;
 }
 
@@ -107,21 +95,19 @@ reference_haversine ( coordinate coord1, coordinate coord2, f64 earth_radius)
     f64 delta_lat = degrees_to_radians(coord2.latitude - coord1.latitude); 
     f64 delta_lon = degrees_to_radians(coord2.longitude - coord1.longitude);
 
-    coord1.latitude = degrees_to_radians(coord1.latitude);
-    coord2.latitude = degrees_to_radians(coord2.latitude);
+    f64 lat1 = degrees_to_radians(coord1.latitude);
+    f64 lat2 = degrees_to_radians(coord2.latitude);
 
 #if 1
-    f64 a = mypow(mysin(delta_lat/2.0), 2.0) + 
-            mycos(coord1.latitude) * mycos(coord2.latitude) * 
-            mypow(mysin(delta_lon/2.0), 2.0); 
+    f64 a = square(mysin(delta_lat/2.0)) +
+            mycos(lat1) * mycos(lat2) * square(mysin(delta_lon/2.0)); 
 
     f64 c = 2.0 * myasin(mysqrt(a));
 #endif
 
 #if 0
-    f64 a = pow(sin(delta_lat/2.0), 2.0) + 
-            cos(coord1.latitude) * cos(coord2.latitude) * 
-            pow(sin(delta_lon/2.0), 2.0); 
+    f64 a = square(sin(delta_lat/2.0)) +
+            cos(lat1) * cos(lat2) * square(sin(delta_lon/2.0)); 
 
     f64 c = 2.0 * asin(sqrt(a));
 
@@ -137,7 +123,6 @@ print_math_function_domains ()
 {
     printf("sin : \tmin: %f\tmax: %f\n", min_sin, max_sin);
     printf("cos : \tmin: %f\tmax: %f\n", min_cos, max_cos);
-    printf("pow : \tmin: %f\tmax: %f\n", min_pow, max_pow);
     printf("sqrt: \tmin: %f\tmax: %f\n", min_sqrt, max_sqrt);
     printf("asin: \tmin: %f\tmax: %f\n", min_asin, max_asin);
 }
